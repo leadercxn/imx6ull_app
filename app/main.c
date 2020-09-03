98d054275e13707a07c6a42e05c2b22de07ad1dc
myubuntu <1012353276@qq.com>
2020-09-04 00:21:24 +0800

    完成epoll实验

--- app/main.c
+++ app/main.c
@@ -9,11 +9,14 @@
 #include <signal.h>
 #include <poll.h>
 #include <sys/select.h>
+#include <sys/epoll.h>
 
 //用户自定义头文件
 #define TRACE_MODULE    "main.c"
 #include <trace.h>
 #include <user_util.h>
+
+#define MAX_EVENTS	10
 
 int main(int argc , char *argv[])
 {
@@ -23,8 +26,13 @@ int main(int argc , char *argv[])
 	uint8_t read_buf[255];
 	int read_sz = 0;
 
-	fd_set readfds;
-	struct timeval timeout;
+	int epoll_fd;
+    epoll_fd = epoll_create(MAX_EVENTS);        //创建一个epoll句柄
+
+	struct epoll_event e_event;					//用来装关注的文件描述符[in]
+	struct epoll_event e_events[MAX_EVENTS];	//用来装底层输出的文件描述符集[out]
+
+	int nfds = 0;								//装epoll返回的文件事件数
 
 	if(argc != 2)
 	{
@@ -39,43 +47,52 @@ int main(int argc , char *argv[])
 		return -1;
 	}
 
-		FD_ZERO(&readfds);	
-		FD_SET(fd,&readfds);
+	e_event.events = EPOLLIN;	//关注的事件类型
+	e_event.data.fd = fd;		//关注的文件描述符
+
+	err_code =  epoll_ctl(epoll_fd,EPOLL_CTL_ADD,fd,&e_event);
+	if(err_code != 0)
+	{
+		trace_errorln("epoll_ctl error");
+
+		return -1;
+	}
 
 	while(1)
 	{
-
-
-
-		timeout.tv_sec = 2;
-		timeout.tv_usec = 0;	//500ms
-
-		/**
-		 * select 超时会清掉所关注的 readfds ，所以每次超时之后都要重置
-		 */
-//		FD_ZERO(&readfds);	
-//		FD_SET(fd,&readfds);
-//		err_code = select((fd + 1), &readfds, NULL, NULL, &timeout);
-		err_code = select((fd + 1), &readfds, NULL, NULL, NULL);	//永远在等待
-
-		if(err_code > 0)
+		nfds = epoll_wait(epoll_fd,e_events,MAX_EVENTS,-1);   //-1永远等待
+		if(nfds)
 		{
-			if(FD_ISSET(fd, &readfds))
-			{
-				trace_infoln("%s can be read",argv[1]);
-				memset(read_buf,0,sizeof(read_buf));
+			trace_infoln("%s can be read",argv[1]);
+			memset(read_buf,0,sizeof(read_buf));
 
-				read_sz = read(fd,read_buf,sizeof(read_buf));		//必须启动读函数，让底层的阻塞的条件复位为false
-				if(read_sz > 0)
+			int i;
+            for(i = 0; i < nfds; i++)
+			{
+				if(e_events[i].data.fd == fd)
 				{
-					trace_debug("receive event queue %d data:",read_sz);
-					trace_dump(read_buf,read_sz);
-				}
-				else
-				{
-					trace_debugln("no any data in event queue");
-				}
-			}
+#if 0
+                    // 向epoll注册  other_fd 监听事件
+                    ev.events=EPOLLIN;
+                    ev.data.fd=connect_fd;
+                    if(epoll_ctl(epoll_fd,EPOLL_CTL_ADD,connect_fd,&ev)==-1)
+                    {
+                        perror("epoll_ctl:connect_fd register failed");
+                        return -1;
+                    }
+#endif
+					read_sz = read(e_events[i].data.fd,read_buf,sizeof(read_buf));		//必须启动读函数，让底层的阻塞的条件复位为false
+					if(read_sz > 0)
+					{
+						trace_debug("receive event queue %d data:",read_sz);
+						trace_dump(read_buf,read_sz);
+					}
+					else
+					{
+						trace_debugln("no any data in event queue");
+					}
+          		}
+        	}
 		}
 		else if(err_code == 0)
 		{
