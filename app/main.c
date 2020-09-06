0947f4887b6378d5679d66fe2776633828affafd
myubuntu <1012353276@qq.com>
2020-09-07 00:19:09 +0800

    新增：应用层异步通知demo

--- app/main.c
+++ app/main.c
@@ -10,6 +10,7 @@
 #include <poll.h>
 #include <sys/select.h>
 #include <sys/epoll.h>
+#include <fcntl.h>
 
 //用户自定义头文件
 #define TRACE_MODULE    "main.c"
@@ -17,25 +18,32 @@
 #include <user_util.h>
 
 #define MAX_EVENTS	10
+static 	int fd;
 
-int main(int argc , char *argv[])
+static void  signal_handler(int sig)
 {
-	int err_code = 0;
-	int fd;
-
 	uint8_t read_buf[255];
 	int read_sz = 0;
 
-	int epoll_fd;
-    epoll_fd = epoll_create(MAX_EVENTS);        //创建一个epoll句柄
+	read_sz = read(fd,read_buf,sizeof(read_buf));		//必须启动读函数，让底层的阻塞的条件复位为false
+	if(read_sz > 0)
+	{
+		trace_debug("receive event queue %d data:",read_sz);
+		trace_dump(read_buf,read_sz);
+	}
+	else
+	{
+		trace_debugln("no any data in event queue");
+	}
+}
 
-	struct epoll_event e_event;					//用来装关注的文件描述符[in]
-	struct epoll_event e_events[MAX_EVENTS];	//用来装底层输出的文件描述符集[out]
-
-	int nfds = 0;								//装epoll返回的文件事件数
+int main(int argc , char *argv[])
+{
+	int flags = 0;
 
 	if(argc != 2)
 	{
+		trace_errorln("Invalid parameter");
 		return -1;
 	}
 
@@ -47,114 +55,18 @@ int main(int argc , char *argv[])
 		return -1;
 	}
 
-	e_event.events = EPOLLIN;	//关注的事件类型
-	e_event.data.fd = fd;		//关注的文件描述符
+	fcntl(fd,F_SETOWN,getpid());		//获取文件描述符异步I/O所有权
+	flags = fcntl(fd,F_GETFL);			//获得文件状态标记
+	fcntl(fd,F_SETFL,flags | FASYNC);	//设置文件状态标记
 
-	err_code =  epoll_ctl(epoll_fd,EPOLL_CTL_ADD,fd,&e_event);
-	if(err_code != 0)
-	{
-		trace_errorln("epoll_ctl error");
-
-		return -1;
-	}
+	signal(SIGIO,signal_handler);		//设置回调函数
 
 	while(1)
 	{
-		nfds = epoll_wait(epoll_fd,e_events,MAX_EVENTS,-1);   //-1永远等待
-		if(nfds)
-		{
-			trace_infoln("%s can be read",argv[1]);
-			memset(read_buf,0,sizeof(read_buf));
-
-			int i;
-            for(i = 0; i < nfds; i++)
-			{
-				if(e_events[i].data.fd == fd)
-				{
-#if 0
-                    // 向epoll注册  other_fd 监听事件
-                    ev.events=EPOLLIN;
-                    ev.data.fd=connect_fd;
-                    if(epoll_ctl(epoll_fd,EPOLL_CTL_ADD,connect_fd,&ev)==-1)
-                    {
-                        perror("epoll_ctl:connect_fd register failed");
-                        return -1;
-                    }
-#endif
-					read_sz = read(e_events[i].data.fd,read_buf,sizeof(read_buf));		//必须启动读函数，让底层的阻塞的条件复位为false
-					if(read_sz > 0)
-					{
-						trace_debug("receive event queue %d data:",read_sz);
-						trace_dump(read_buf,read_sz);
-					}
-					else
-					{
-						trace_debugln("no any data in event queue");
-					}
-          		}
-        	}
-		}
-		else if(err_code == 0)
-		{
-			trace_warnln("wait timeout");
-		}
-		else if(err_code < 0)
-		{
-			trace_errorln("wait error");
-		}
+		trace_infoln("i am alive");
+		sleep(2);
 	}
 
-#if 0
-	struct pollfd fds;
-
-	if(argc != 2)
-	{
-		return -1;
-	}
-
-	fd = open(argv[1],O_RDWR);		//阻塞式访问
-	if(fd < 0)
-	{
-		trace_errorln("open %s file error",argv[1]);
-
-		return -1;
-	}
-
-	fds.fd = fd;
-	fds.events = POLLIN;		//poll关注的事件
-
-	while(1)
-	{
-		/**
-		 * -1 为一直等待
-		 */
-		err_code  = poll(&fds,sizeof(fds)/sizeof(struct pollfd),(-1));
-		if(err_code)
-		{
-			trace_infoln("%s can be read",argv[1]);
-			memset(read_buf,0,sizeof(read_buf));
-
-			read_sz = read(fds.fd,read_buf,sizeof(read_buf));		//必须启动读函数，让底层的阻塞的条件复位为false
-			if(read_sz > 0)
-			{
-				trace_debug("receive event queue %d data:",read_sz);
-				trace_dump(read_buf,read_sz);
-			}
-			else
-			{
-				trace_debugln("no any data in event queue");
-			}
-		}
-		else if(err_code == 0)
-		{
-			trace_warnln("wait timeout");
-		}
-		else if(err_code < 0)
-		{
-			trace_errorln("wait error");
-		}
-	}
-#endif
 	close(fd);
 	return 0;
 }
